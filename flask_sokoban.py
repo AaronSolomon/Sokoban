24c0e3f229ec66de5258d857e99c232062c36d0f
Solomon <solomon@mail.ncnu.edu.tw>
2025-01-29 14:46:52 +0800

    Assign level by queryString

--- flask_sokoban.py
+++ flask_sokoban.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('sokoban.html')
+    level = int(request.values.get('level', '1'))
+    #board="  HHH\n  HDH\n  H HHHH\nHHHB BDH\nHD BW HH\nHHHHBHH\n   HDH\n   HHH\n"
+    return render_template('sokoban.html', level=level, board=readBoard(level))
+
+def readBoard(level):
+    fn = 'Boards/map{:03}.txt'.format(level)
+    with open(fn) as infile:
+        s = infile.read()
+    return s
 
 if __name__ == "__main__":
     app.run(debug=True)
