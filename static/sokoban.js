9a454d77fe4f32b3ed7a78457b84e63a154fbca1
Solomon <solomon@mail.ncnu.edu.tw>
2025-05-03 21:06:47 +0800

    Press <Zoom In> to resize.

--- static/sokoban.js
+++ static/sokoban.js
@@ -31,6 +31,8 @@ if ('username' in cookies) {
   updateUsername(username);
 }
 document.getElementById('editUsername').addEventListener('click', editUsername);
+document.getElementById('zoomIn').addEventListener('click', zoomIn);
+// document.getElementById('scale').addEventListener('click', scale);
 
 // Check whether the server sends a greeting message via cookie
 if ('msg' in cookies) {
@@ -228,16 +230,6 @@ function showBoard_G(m) {
     html += '</p>\n';
     document.getElementById("level").innerHTML = m.level;
     document.getElementById("boardArea").innerHTML = html;
-    // Auto-resize
-    // let width = window.innerWidth - 32*2;
-    // let height = window.innerHeight - 32*10;
-    // let scale = Math.min( (width / 32 / m.width),
-    //     (height / 32 / rows.length ) );    // BMP file is 32x32
-    // document.body.style.zoom = scale;
-    // alert("Width: " + width + " Height: " + height +
-    // " Scale: " + scale +
-    // " xScale: " + Math.floor(width / 32 / m.width) + " Height: " + 
-    // Math.floor(height / 32 / rows.length ) );
 }
 
 function k(e) {
@@ -476,4 +468,34 @@ function detectSwipeDirection() {
             moveWorker(myBoard, S_UP);
         }
     }
+}
+
+function scale() {
+  let m = myBoard;
+  const rows = m.board;
+  let width = window.innerWidth;
+  // let height = window.innerHeight;
+  let height = window.innerHeight - 32*5; // Some heights are for buttons
+  let scale = Math.min( (width / 32 / m.width),
+       (height / 32 / rows.length ) );    // BMP file is 32x32
+  let msg = "width:"+window.innerWidth + " height:" + window.innerHeight;
+  msg += "\nMap width:"+m.width+ " Height:"+m.height;
+  msg += "\nScale:" + scale;
+  alert(msg);
+  return scale;
+  // let width = window.innerWidth - 32*2;
+  // alert("Width: " + width + " Height: " + height +
+  // " Scale: " + scale +
+  // " xScale: " + Math.floor(width / 32 / m.width) + " Height: " + 
+  // Math.floor(height / 32 / rows.length ) );
+}
+
+function zoomIn() {
+  // document.body.style.zoom = "1.5";
+  // let s = scale();
+  // let msg = "Scale:" + s;
+  // alert(msg);
+  // Auto-resize
+  // document.body.style.zoom = s;
+  document.body.style.zoom = scale() * 0.9;
 }
